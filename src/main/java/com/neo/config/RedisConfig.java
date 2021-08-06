ebac38f570888570fc040a21c6cbf6b42daccfac
Yoga2Pro <Yoga2Pro@users.noreply.github.com>
2021-08-05 19:09:59 -0700

    add ttl to redis, and remove session for now

--- src/main/java/com/neo/config/RedisConfig.java
+++ src/main/java/com/neo/config/RedisConfig.java
@@ -1,18 +1,21 @@
 package com.neo.config;
 
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import java.lang.reflect.Method;
-
+import java.time.Duration;
 
 @Configuration
 @EnableCaching
 public class RedisConfig extends CachingConfigurerSupport{
-	
 	@Bean
 	public KeyGenerator keyGenerator() {
         return new KeyGenerator() {
@@ -27,5 +30,35 @@ public class RedisConfig extends CachingConfigurerSupport{
                 return sb.toString();
             }
         };
+    }
+
+    @Bean
+    public CacheManager cacheManager(RedisTemplate<Object, Object> template) {
+
+        // 基本配置
+        RedisCacheConfiguration defaultCacheConfiguration =
+                RedisCacheConfiguration
+                        .defaultCacheConfig()
+                        // 设置key为String
+                        .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(template.getStringSerializer()))
+                        // 设置value 为自动转Json的Object
+                        .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(template.getValueSerializer()))
+                        // 不缓存null
+                        .disableCachingNullValues()
+                        // 缓存数据保存1小时
+                        .entryTtl(Duration.ofMinutes(1));
+
+        // 够着一个redis缓存管理器
+        RedisCacheManager redisCacheManager =
+                RedisCacheManager.RedisCacheManagerBuilder
+                        // Redis 连接工厂
+                        .fromConnectionFactory(template.getConnectionFactory())
+                        // 缓存配置
+                        .cacheDefaults(defaultCacheConfiguration)
+                        // 配置同步修改或删除 put/evict
+                        .transactionAware()
+                        .build();
+
+        return redisCacheManager;
     }
 }

da87c29ef61267654bf856a77121c637e2b83e22
Yoga2Pro <Yoga2Pro@users.noreply.github.com>
2021-08-10 09:14:59 -0700

    lock pom version, add table index

--- src/main/resources/migration/V1__Base_version.sql
+++ src/main/resources/migration/V1__Base_version.sql
@@ -3,5 +3,7 @@ CREATE TABLE `users` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(20) NOT NULL,
   `age` int(5) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `email` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `index_email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
